a943edfc55d1ba259086425efd9d4849203df953
snOlga <olga.safonova@outlook.com>
2024-05-01 23:20:48 +0300

    ok, just another try

--- Test.cs
+++ Test.cs
@@ -3,64 +3,68 @@ using Serilog;
 
 public class Test
 {
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_if.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_if.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\if.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_loop.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_loop.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\loop.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_math.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_math.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\math.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_swap.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_swap.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\swap.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_string_const.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_string_const.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\string_const.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_variables.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_variables.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\variables.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_dup.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_dup.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\dup.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_rot.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_rot.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\rot.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_simple_procedure.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_simple_procedure.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\simple_procedure.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_fibonacci.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_fibonacci.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\fibonacci.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_fibonacci_procedure.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_fibonacci_procedure.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\fibonacci_procedure.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_hello_world.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_hello_world.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\hello_world.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_hello_user.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_hello_user.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\hello_user.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input_user.txt")]
-    [TestCase("D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\test_cat.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\results\\result_cat.txt",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\logs\\cat.log",
-                "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\inputs\\input_cat.txt")]
+    [TestCase("\\test_if.txt",
+                "\\results\\result_if.txt",
+                "\\logs\\if.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_loop.txt",
+                "\\results\\result_loop.txt",
+                "\\logs\\loop.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_math.txt",
+                "\\results\\result_math.txt",
+                "\\logs\\math.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_swap.txt",
+                "\\results\\result_swap.txt",
+                "\\logs\\swap.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_string_const.txt",
+                "\\results\\result_string_const.txt",
+                "\\logs\\string_const.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_variables.txt",
+                "\\results\\result_variables.txt",
+                "\\logs\\variables.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_dup.txt",
+                "\\results\\result_dup.txt",
+                "\\logs\\dup.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_rot.txt",
+                "\\results\\result_rot.txt",
+                "\\logs\\rot.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_simple_procedure.txt",
+                "\\results\\result_simple_procedure.txt",
+                "\\logs\\simple_procedure.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_fibonacci.txt",
+                "\\results\\result_fibonacci.txt",
+                "\\logs\\fibonacci.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_fibonacci_procedure.txt",
+                "\\results\\result_fibonacci_procedure.txt",
+                "\\logs\\fibonacci_procedure.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_hello_world.txt",
+                "\\results\\result_hello_world.txt",
+                "\\logs\\hello_world.log",
+                "\\inputs\\input.txt")]
+    [TestCase("\\test_hello_user.txt",
+                "\\results\\result_hello_user.txt",
+                "\\logs\\hello_user.log",
+                "\\inputs\\input_user.txt")]
+    [TestCase("\\test_cat.txt",
+                "\\results\\result_cat.txt",
+                "\\logs\\cat.log",
+                "\\inputs\\input_cat.txt")]
     public void TestsNoInput(string testPath, string resultPath, string logging, string input)
     {
+        testPath = Directory.GetCurrentDirectory() + "\\tests" + testPath;
+        resultPath = Directory.GetCurrentDirectory() + "\\tests" + resultPath;
+        logging = Directory.GetCurrentDirectory() + "\\tests" + logging;
+        input = Directory.GetCurrentDirectory() + "\\tests" + input;
         File.WriteAllText(logging, string.Empty);
 
         Log.Logger = new LoggerConfiguration()
@@ -72,7 +76,7 @@ public class Test
         Log.Information("Imitation started");
 
         Memory memory = new Memory();
-        DataPath dataPath = new DataPath(memory, input, "D:\\ITMO\\2_year\\csa\\ForthImitation\\tests\\output.txt");
+        DataPath dataPath = new DataPath(memory, input, "\\output.txt");
         ControlUnit controlUnit = new ControlUnit(testPath,
                                                 "D:\\ITMO\\2_year\\csa\\ForthImitation\\microcode.txt", dataPath);
 
